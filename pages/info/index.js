d8a37aaef496b47cb4695bfb240300ca54b429ee
sidhanathdon <sid55boss@gmail.com>
2023-12-08 20:46:50 +0530

    changes updated

--- pages/info/index.js
+++ pages/info/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import Link from 'next/link';
-
+import React from "react";
+import Link from "next/link";
 const Info = () => {
-  return (
-    <div id='info-page'>
-      <h1> Info page </h1>
-      <h2>I am born in India</h2>
-      <h2>and I like to play Cricket </h2>
-      <button id='info-to-blog'> Go to Blog</button>
-      <button id='info-to-home'> Go to Home</button>
-    </div>
-  );
-};
-export default Info;
+
+
+    return (
+        <div id="info-page">
+
+
+            <h1> Info page </h1>
+            <h2>I am born in India</h2>
+            <h2>and I like to play Cricket </h2>
+            <Link href="/blog"><button id="info-to-blog"> Go to Blog</button></Link>
+            <Link href="/"><button id="info-to-home"> Go to Home</button></Link>
+        </div>
+    )
+}
+export default Info
